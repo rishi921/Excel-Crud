5956a507c0dc2498ad5e77b78c49487aa4d62b53
rishi921 <shrishidurga21@gmail.com>
2024-09-24 13:00:10 +0530

    Created Project

---
+++ ExcelCRUDWebAPI/Controllers/DatasetsController.cs
@@ -0,0 +1,47 @@
+﻿using ExcelCRUDWebAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+using ExcelCRUDWebAPI.Services;
+using ExcelCRUDWebAPI.Models;
+namespace ExcelCRUDWebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DatasetsController : ControllerBase
+    {
+        private readonly ExcelServices _ExcelServices;
+
+        public DatasetsController(ExcelServices excelServices)
+        {
+            _ExcelServices = excelServices;
+
+        }
+
+        [HttpGet]
+        public ActionResult<List<PersonalInfo>> Get()
+        {
+            return _ExcelServices.ReadExcelData();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] PersonalInfo info)
+        {
+            _ExcelServices.AddExcelData(info);
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] PersonalInfo info)
+        {
+            _ExcelServices.UpdateExcelData(info);
+            return Ok();
+        }
+
+        [HttpDelete("{aadharCardNumber}")]
+        public IActionResult Delete(string aadharCardNumber)
+        {
+            _ExcelServices.DeleteExcelData(aadharCardNumber);
+            return Ok();
+        }
+    }
+
+}
